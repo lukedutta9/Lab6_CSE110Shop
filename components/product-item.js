4297ac3b269c808bd2397ee3f24684fc6e56a828
Luke Dutta <lukedutta9@gmail.com>
2021-02-13 19:11:33 -0800

    finish lab

--- components/product-item.js
+++ components/product-item.js
@@ -1,7 +1,159 @@
 // product-item.js
 
+let store = window.localStorage;
+let cart = store.getItem('cart');
+let cartSet;
+
+if (cart === null || cart.toString() == "") {
+  cartSet = [];
+} else {
+  cartSet = cart.split(",");
+}
+
 class ProductItem extends HTMLElement {
-  // TODO
+
+  constructor() {
+    super();
+
+    const shadow = this.attachShadow({mode: 'open'});
+
+    // style
+
+    shadow.innerHTML = `
+    <style>
+      .price {
+        color: green;
+        font-size: 1.8em;
+        font-weight: bold;
+        margin: 0;
+      }
+      
+      .product {
+        align-items: center;
+        background-color: white;
+        border-radius: 5px;
+        display: grid;
+        grid-template-areas: 
+        'image'
+        'title'
+        'price'
+        'add';
+        grid-template-rows: 67% 11% 11% 11%;
+        height: 450px;
+        filter: drop-shadow(0px 0px 6px rgb(0,0,0,0.2));
+        margin: 0 30px 30px 0;
+        padding: 10px 20px;
+        width: 200px;
+      }
+      
+      .product > button {
+        background-color: rgb(255, 208, 0);
+        border: none;
+        border-radius: 5px;
+        color: black;
+        justify-self: center;
+        max-height: 35px;
+        padding: 8px 20px;
+        transition: 0.1s ease all;
+      }
+      
+      .product > button:hover {
+        background-color: rgb(255, 166, 0);
+        cursor: pointer;
+        transition: 0.1s ease all;
+      }
+      
+      .product > img {
+        align-self: center;
+        justify-self: center;
+        width: 100%;
+      }
+      
+      .title {
+        font-size: 1.1em;
+        margin: 0;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+      }
+      
+      .title:hover {
+        font-size: 1.1em;
+        margin: 0;
+        white-space: wrap;
+        overflow: auto;
+        text-overflow: unset;
+      }
+    </style>`;
+
+    // li
+    const product = document.createElement('li');
+    product.setAttribute('class', 'product')
+
+    // img
+    const image = product.appendChild(document.createElement('img'));
+    image.alt = this.getAttribute('alt');
+    image.width = 200;
+
+    // p - title
+    const title = product.appendChild(document.createElement('p'));
+    title.setAttribute('class', 'title');
+
+    // p2 - price
+    const price = product.appendChild(document.createElement('p'));
+    price.setAttribute('class', 'price');
+
+    // button
+    const button = product.appendChild(document.createElement('button'));
+    button.setAttribute('onclick', "addOrRemoveCart(event)");
+    button.innerText = 'Add to Cart';
+
+    shadow.append(product);
+  }
+
+  attributeChangedCallback(attrName, oldVal, newVal) {
+    const prod = this.shadowRoot.lastChild;
+    const childs = prod.children;
+    if (attrName == 'src') {
+      const image = childs[0];
+      image.src = newVal;
+    } else if (attrName == 'alt') {
+      const image = childs[0];
+      image.alt = newVal;
+    } else if (attrName == 'title') {
+      const title = childs[1];
+      title.textContent = newVal;
+    } else if (attrName == 'id') {
+      const cartCount = document.getElementById('cart-count');
+      const button = childs[3];
+      let isBut = false;
+
+      for (let i = 0; i < cartSet.length; i++) {
+        if (cartSet[i] == newVal) {
+          isBut = true;
+        }
+      }
+      button.setAttribute("num", newVal);
+      if (isBut) {
+        button.textContent = "Remove from Cart";
+        button.setAttribute("incart", true);
+        cartCount.textContent = Number(cartCount.textContent) + 1;
+      } else {
+        button.textContent = "Add to Cart";
+        button.removeAttribute("incart");
+      }
+    } else {
+      const price = childs[2];
+      price.textContent = '$' + newVal;
+    }
+  }
+
+  static get observedAttributes() { 
+    return ['title', 'src', 'alt', 'price', 'id']; 
+  }
+
+  connectedCallback() {
+  }
 }
 
 customElements.define('product-item', ProductItem);
--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,62 @@
 // Script.js
 
-window.addEventListener('DOMContentLoaded', () => {
-  // TODO
+window.addEventListener('DOMContentLoaded', async () => {
+  let store = window.localStorage;
+  let items = null;
+
+  let cart = store.getItem('cart');
+  let cartSet = null;
+  
+  if (cart === null || cart.toString() == "") {
+    cartSet = [];
+  } else {
+    cartSet = cart.split(",");
+  }
+
+  console.log(cartSet.toString() === "");
+
+  if (store.getItem('itemsArray') === null) {
+    console.log("fetching");
+    await fetch('https://fakestoreapi.com/products')
+      .then(response => response.json())
+      .then(data => items = data)
+      .then(() => store.setItem('itemsArray', JSON.stringify(items)));
+  } else {
+    items = JSON.parse(store.getItem('itemsArray'));
+  }
+  
+  const productList = document.getElementById('product-list')
+
+  for (let i = 0; i < items.length; i++) {
+    const product = items[i];
+    const itm = productList.appendChild(document.createElement('product-item'));
+    itm.setAttribute('src', product.image);
+    itm.setAttribute('alt', product.title);
+    itm.setAttribute('title', product.title);
+    itm.setAttribute('price', product.price);
+    itm.setAttribute('id', product.id);
+  }
 });
+
+function addOrRemoveCart(ev) {
+  const button = ev.explicitOriginalTarget;
+  const cartCount = document.getElementById('cart-count');
+
+  if (!button.hasAttribute("incart")) {
+    button.textContent = "Remove from Cart";
+    button.setAttribute("incart", true);
+    cartSet.push(button.getAttribute("num"));
+    cartCount.textContent = Number(cartCount.textContent) + 1;
+  } else {
+    button.textContent = "Add to Cart";
+    button.removeAttribute("incart");
+    const index = cartSet.indexOf(button.getAttribute("num"));
+    if (index > -1) { 
+      let remove = cartSet.splice(index, 1);
+    }
+    cartCount.textContent = Number(cartCount.textContent) - 1;
+  }
+
+  console.log(cartSet);
+  store.setItem('cart', cartSet.toString());
+}
